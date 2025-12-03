76747ea014e235ba1fa357df67b354f6d0f4152c
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-12-03 12:19:42 +0800

    Add HTTP methods for column CRUD operations
    Introduced createColumn, updateColumn, and deleteColumn methods to the Column service using Angular's HttpClient. These methods enable API interactions for creating, updating, and deleting columns.

--- frontend/src/app/features/boards/services/column.ts
+++ frontend/src/app/features/boards/services/column.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ColumnRequest } from '../models/board';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Column {
-  
+  private http = inject(HttpClient);
+  private readonly API_URL = 'https://localhost:5287/api/column';
+
+  createColumn(boardId: number, request: ColumnRequest): Observable<Column> {
+    return this.http.post<Column>(`${this.API_URL}/${boardId}`, request);
+  }
+
+  updateColumn(id: number, request: ColumnRequest): Observable<Column> {
+    return this.http.put<Column>(`${this.API_URL}/${id}`, request);
+  }
+
+  deleteColumn(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
 }
